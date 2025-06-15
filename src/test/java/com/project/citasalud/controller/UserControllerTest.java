e40b4064c7fd12a46dfd6689730a0a2526f45154
miguelvelezsk <miguelangel.velez@udea.edu.co>
2025-06-15 12:38:31 -0500

    bug correction

--- src/test/java/com/project/citasalud/controller/UserControllerTest.java
+++ src/test/java/com/project/citasalud/controller/UserControllerTest.java
@@ -4,20 +4,20 @@ import com.project.citasalud.jwt.JwtAuthFilter;
 import com.project.citasalud.jwt.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(controllers = UserController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class UserControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private JwtService jwtService;
 
